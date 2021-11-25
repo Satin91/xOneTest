0c63ce40418d8a032188fb66671c76f9c729d764
Arthur <cool19912015@gmail.com>
2021-11-25 22:31:00 +0300

    Initial Commit

---
+++ xOneTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  xOneTest
+//
+//  Created by Артур on 25.11.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
